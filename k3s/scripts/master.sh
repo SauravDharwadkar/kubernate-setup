e71ce476658fab6415886e0e4fd01d94d3a5e274
sauravdharwadkar <dharwadkarsaurav@gmail.com>
2022-07-30 09:49:34 +0530

    fixed TOKEN

--- k3s/scripts/master.sh
+++ k3s/scripts/master.sh
@@ -3,4 +3,5 @@
 # using  https://k3s.io
 
 curl -sfL https://get.k3s.io | sh -
-echo "export TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)" >> /vagrant_data/data/nodeENV
+mkdir -p /vagrant_data/data
+sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant_data/data/nodeENV
--- k3s/scripts/node.sh
+++ k3s/scripts/node.sh
@@ -3,11 +3,11 @@
 
 while true ; do
 if test -f "/vagrant_data/data/nodeENV"; then
+
+curl -sfL https://get.k3s.io | K3S_URL=https://master-node:6443 K3S_TOKEN=$(cat /vagrant_data/data/nodeENV) sh -
     break
 fi
 echo "waiting for Master file"
 sleep 10
 done
 
-. /vagrant_data/data/nodeENV
-curl -sfL https://get.k3s.io | K3S_URL=https://master-node:6443 K3S_TOKEN=$TOKEN sh -
