3ab2a254dd50bd549dbba0ed43d39e72f55c115c
sauravdharwadkar <dharwadkarsaurav@gmail.com>
2022-07-30 11:36:18 +0530

    added dashboard and fixed ip

--- k3s/scripts/master.sh
+++ k3s/scripts/master.sh
@@ -6,4 +6,5 @@ curl -sfL https://get.k3s.io | sh -
 mkdir -p /vagrant_data/data
 sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant_data/data/nodeENV
 sudo cp /etc/rancher/k3s/k3s.yaml /vagrant_data/data/k3s.yaml 
-sed -s "s/127.0.0.1/${IP_MASTER}/g" data/k3s.yaml
+echo "changing ip : 127.0.0.1 to $IP_MASTER"
+sed -i -s "s/127.0.0.1/${IP_MASTER}/g" /vagrant_data/data/k3s.yaml
