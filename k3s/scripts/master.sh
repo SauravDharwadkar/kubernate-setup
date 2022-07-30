c32692eb6b6b1f649f35ffd303e0063810bea16f
sauravdharwadkar <dharwadkarsaurav@gmail.com>
2022-07-30 11:02:41 +0530

    copy kubectl config to data

--- k3s/scripts/master.sh
+++ k3s/scripts/master.sh
@@ -5,3 +5,5 @@
 curl -sfL https://get.k3s.io | sh -
 mkdir -p /vagrant_data/data
 sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant_data/data/nodeENV
+sudo cp /etc/rancher/k3s/k3s.yaml /vagrant_data/data/k3s.yaml 
+sed -s "s/127.0.0.1/${IP_MASTER}/g" data/k3s.yaml
